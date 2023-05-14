a73e11dddc758984c8a4941240a4a653ec4e4641
lazywalker <lazywalkerz@gmail.com>
2023-05-14 08:10:33 +0800

    use mini.pairs instead of nvim-autopairs

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -18,33 +18,15 @@ return {
   },
 
   {
-    "windwp/nvim-autopairs",
+    'echasnovski/mini.pairs',
+    version = '*',
     event = "BufReadPost",
     init = function()
-      local npairs = require("nvim-autopairs")
-      local Rule = require('nvim-autopairs.rule')
-
-      npairs.setup {
-        check_ts = true,
-        ts_config = {
-          lua = { 'string' }, -- it will not add a pair on that treesitter node
-          javascript = { 'template_string' },
-          java = false,       -- don't check treesitter on java
-        }
-      }
-
-      local ts_conds = require('nvim-autopairs.ts-conds')
-      -- press % => %% only while inside a comment or string
-      npairs.add_rules {
-        Rule("%", "%", "lua")
-            :with_pair(ts_conds.is_ts_node({ 'string', 'comment' })),
-        Rule("$", "$", "lua")
-            :with_pair(ts_conds.is_not_ts_node({ 'function' }))
-      }
+      require("mini.pairs").setup {}
     end
   },
 
-  -- Vim plugin for automatically highlighting other uses of the word 
+  -- Vim plugin for automatically highlighting other uses of the word
   -- under the cursor using either LSP, Tree-sitter, or regex matching.
   {
     "RRethy/vim-illuminate",
